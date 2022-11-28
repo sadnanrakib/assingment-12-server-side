a93947b8c73581c8d3da28ce10a47ed4782dd478
sadnanrakib <sadnanrakib02@gmail.com>
2022-11-28 20:05:37 +0600

    add

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 // J2wHd32HmhcJ_Lm
 
 app.use(cors());
-// app.use(express.json());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tsk4obg.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(process.env.DB_USER);
